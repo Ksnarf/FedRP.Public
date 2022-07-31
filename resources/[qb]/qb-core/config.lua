3ffd19b3b80c4eae00715e9ce15dee09311a0eca
Ksnarf <44852455+Ksnarf@users.noreply.github.com>
2022-07-31 10:54:08 -0700

    test update #2
    with git autodeployment

--- resources/[qb]/qb-core/config.lua
+++ resources/[qb]/qb-core/config.lua
@@ -3,7 +3,7 @@ QBConfig = {}
 QBConfig.MaxPlayers = GetConvarInt('sv_maxclients', 10) -- Gets max players from config file, default 48
 QBConfig.DefaultSpawn = vector4(-1035.71, -2731.87, 12.86, 0.0)
 QBConfig.UpdateInterval = 5 -- how often to update player data in minutes
-QBConfig.StatusInterval = 5000 -- how often to check hunger/thirst status in milliseconds
+QBConfig.StatusInterval = 10000 -- how often to check hunger/thirst status in milliseconds
 
 QBConfig.Money = {}
 QBConfig.Money.MoneyTypes = { cash = 500, bank = 5000, crypto = 0 } -- type = startamount - Add or remove money types for your server (for ex. blackmoney = 0), remember once added it will not be removed from the database!
