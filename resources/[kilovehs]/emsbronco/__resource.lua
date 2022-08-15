a76540487908c1840dc92a82473804e177b378ac
Ksnarf <44852455+Ksnarf@users.noreply.github.com>
2022-08-14 17:57:30 -0700

    update emsbronco id's

--- resources/[kilovehs]/emsbronco/__resource.lua
+++ resources/[kilovehs]/emsbronco/__resource.lua
@@ -1,16 +1,16 @@
 resource_manifest_version '44febabe-d386-4d18-afbe-5e627f4af937'
 
-data_file 'VEHICLE_LAYOUTS_FILE' 'data/vehiclelayouts.meta'
-data_file 'CARCOLS_FILE' 'data/carcols.meta'
-data_file 'HANDLING_FILE' 'data/handling.meta'
-data_file 'VEHICLE_METADATA_FILE' 'data/vehicles.meta'
-data_file 'VEHICLE_VARIATION_FILE' 'data/carvariations.meta'
+data_file 'VEHICLE_LAYOUTS_FILE' 'vehiclelayouts.meta'
+data_file 'CARCOLS_FILE' 'carcols.meta'
+data_file 'HANDLING_FILE' 'handling.meta'
+data_file 'VEHICLE_METADATA_FILE' 'vehicles.meta'
+data_file 'VEHICLE_VARIATION_FILE' 'carvariations.meta'
 
 
 files {
-	'data/vehiclelayouts.meta',
-	'data/carcols.meta',
-	'data/handling.meta',
-	'data/vehicles.meta',
-	'data/carvariations.meta',
+	'vehiclelayouts.meta',
+	'carcols.meta',
+	'handling.meta',
+	'vehicles.meta',
+	'carvariations.meta',
 }
